23ca49f1057bd14ee9212a0dfc0158489018ab5e
jbkimmm <jbkimmm@gmail.com>
2019-03-09 13:21:13 +0900

    model thumenail image field upload 삭제

--- posts/models.py
+++ posts/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
     comment_count = models.IntegerField(default = 0)
     view_count = models.IntegerField(default = 0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    thumbnail = models.ImageField(upload_to='post_image', blank=True)
+    thumbnail = models.ImageField(blank=True)
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
     previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
